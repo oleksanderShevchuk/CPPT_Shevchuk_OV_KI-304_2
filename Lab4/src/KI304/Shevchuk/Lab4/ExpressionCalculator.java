1ceadfc7fbe41238f3007def1b94716c525a6f62
oleksanderShevchuk <oleksander.shevchuk222@gmail.com>
2023-09-18 17:58:44 +0300

    Lab4: Fixed a couple of bugs.

--- Lab4/src/KI304/Shevchuk/Lab4/ExpressionCalculator.java
+++ Lab4/src/KI304/Shevchuk/Lab4/ExpressionCalculator.java
@@ -4,22 +4,28 @@ package KI304.Shevchuk.Lab4;
  * The {@code ExpressionCalculator} class calculates the result of a given mathematical expression.
  */
 public class ExpressionCalculator {
+
     /**
      * Calculates the result of the expression y = sin(x - 9) / (x - cos(2x)).
-     *
      * @param x The value of x in the expression.
      * @return The calculated result y.
+     * @throws ArithmeticException if division by zero or very close to zero occurs.
      */
-    public static double calculateExpression(double x) {
-        // Calculate the expression y
+    public static double calculateExpression(double x) throws ArithmeticException {
+        // Check if the denominator (x - cos(2x)) is close to zero
         double numerator = Math.sin(x - 9);
         double denominator = x - Math.cos(2 * x);
 
-        // Check for division by zero
-        if (denominator == 0) {
-            throw new ArithmeticException("Division by zero is not allowed.");
+        // Check if the denominator is very close to zero
+        if (Math.abs(denominator) < 1e-10) {
+            throw new ArithmeticException("Division by zero or very close to zero.");
+        }
+        // Check if the numerator is zero
+        else if (numerator == 0) {
+            throw new ArithmeticException("Division with zero in the numerator.");
         }
 
+        // Calculate the expression
         return numerator / denominator;
     }
 }
