22e2c9c9678cda0aaa7941fcde680ae35cd43636
oleksanderShevchuk <oleksander.shevchuk222@gmail.com>
2023-09-28 11:14:41 +0300

    Lab1: Fixed some bugs.

--- Lab1/src/Lab1/Shevchuk/KI304/Lab1ShevchukKI304.java
+++ Lab1/src/Lab1/Shevchuk/KI304/Lab1ShevchukKI304.java
@@ -20,16 +20,21 @@ public class Lab1ShevchukKI304 {
         System.out.print("Enter the size of a square matrix: ");
         nRows = in.nextInt();
         in.nextLine();
-        arr = new char[nRows][];
-        int halfNRows = (nRows / 2) + 1;
-
-        for (int i = 0; i < nRows; i++) {
-            arr[i] = new char[i + 1];
-        }
 
         // Prompt the user for the filler character
-        System.out.print("\nEnter the filling character: ");
+        System.out.print("Enter the filling character: ");
         filler = in.nextLine();
+
+        arr = new char[nRows][];
+        int halfNRows = (nRows / 2) + 1;
+        int row = 1;
+        for (int i = 0; i < halfNRows; i++) {
+            arr[i] = new char[row];
+            for (int j = 0; j < row; j++) {
+                arr[i][j] = (char) filler.codePointAt(0);
+            }
+            row += 2;
+        }
 
         exit:
         if (filler.isEmpty()) {
@@ -38,20 +43,31 @@ public class Lab1ShevchukKI304 {
             System.out.print("\nToo many filler characters entered.");
         } else {
             int counter = halfNRows - 1;
-            for (int i = 0; i < nRows; i++) {
-                for (int j = 0; j < arr[i].length; j++) {
-
-                    if (j >= counter) {
-                        arr[i][j] = (char) filler.codePointAt(0);
+            for (int i = 0; i < halfNRows - 1; i++) {
+                for (int j = 0; j < nRows; j++){
+                    System.out.print("  ");
+                    fout.print("  ");
+                }
+                System.out.print("\n");
+                fout.print("\n");
+            }
+            int colArr = 0, rowArr = 0;
+            for (int i = halfNRows - 1; i < nRows; i++) {
+                for (int j = 0; j < nRows; j++) {
+                    if (j >= counter && arr[rowArr].length > colArr) {
+                        System.out.print(arr[rowArr][colArr] + " ");
+                        fout.print(arr[rowArr][colArr] + " ");
+                        colArr++;
                     } else {
-                        arr[i][j] = ' ';
+                        System.out.print("  ");
+                        fout.print("  ");
                     }
-                    System.out.print(arr[i][j] + " ");
-                    fout.print(arr[i][j] + " ");
                     if (j == counter) {
                         counter--;
                     }
                 }
+                rowArr++;
+                colArr = 0;
                 System.out.print("\n");
                 fout.print("\n");
             }
